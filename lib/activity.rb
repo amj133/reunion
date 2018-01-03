f296ecae82266d03c0cebf5fe62d77d149239301
amj133 <amj@vt.edu>
2018-01-03 09:30:20 -0700

    completed iteration 1 and first half of iteration 2

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,27 @@
+
 class Activity
+  attr_reader :name, :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participants(people)
+    participants.update(people)
+  end
+
+  def total_cost
+    participants.values.sum
+  end
+
+  def split_cost
+    total_cost.to_f / participants.count
+  end
+
+  def amount_owed
+    participants.map do |person|
+      
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -2,6 +2,57 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
+require 'pry'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new("hiking")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_has_a_name
+    activity = Activity.new("hiking")
+
+    assert_equal "hiking", activity.name
+  end
+
+  def test_can_add_a_participant
+    activity = Activity.new("skydiving")
+
+    activity.add_participants({Bob: 165})
+
+    assert_equal [:Bob], activity.participants.keys
+    assert_equal [165], activity.participants.values
+  end
+
+  def test_can_add_multiple_participants
+    activity = Activity.new("skydiving")
+
+    activity.add_participants({Bob: 165, Alice: 215})
+    activity.add_participants({Frodo: 5})
+
+    assert_equal [:Bob, :Alice, :Frodo], activity.participants.keys
+    assert_equal [165, 215, 5], activity.participants.values
+  end
+
+  def test_total_cost_returns_total_cost_of_activity
+    activity = Activity.new("skydiving")
+
+    activity.add_participants({Bob: 165, Alice: 215})
+    activity.add_participants({Frodo: 5})
+
+    assert_equal 385, activity.total_cost
+  end
+
+  def test_split_cost_returns_even_split_of_total_cost
+    activity = Activity.new("skydiving")
+
+    activity.add_participants({Bob: 115, Alice: 85})
+    activity.add_participants({Frodo: 100})
+
+    assert_equal 100, activity.split_cost
+  end
+
 end
